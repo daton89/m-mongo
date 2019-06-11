ccc99d2bf664bf0b14eee64991a8ae43d26ef446
Tony <tonydangelo123@gmail.com>
2019-06-11 23:43:29 +0200

    add few new actions

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,8 +1,12 @@
+export const ADD_SSH_CONNECTION = 'Add SSH connection';
+export const RUN_SSH_COMMAND = 'Run a command remotely';
+export const SET_STORAGE_PATH = 'Set the default storage directory';
 export const SHOW_CLUSTERS = 'Show clusters';
 export const ADD_CLUSTER = 'Add a cluster';
 export const REMOVE_CLUSTER = 'Remove a cluster';
 export const DUMP = 'Dump';
+export const DUMP_OVER_SSH = 'Dump over ssh';
 export const RESTORE = 'Restore';
 export const EXPORT = 'Export';
 export const IMPORT = 'Import';
-export const SET_STORAGE_PATH = 'Set storage directory'
+export const EXIT = 'Exit';
