0c0f8451f2a84b1532072c99a1edd21913ac5571
Tony <tonydangelo123@gmail.com>
2019-06-11 23:45:20 +0200

    add missing fields in cluster interface

--- src/lib/cluster.ts
+++ src/lib/cluster.ts
@@ -1,8 +1,15 @@
+import { ConnectionParams } from './ssh';
 export interface Cluster {
   name: string,
+  runningOn: string,
+  type: string,
+  accessMethod: string,
+  requiresSSH: string,
+  uri: string,
   host: string,
   ssl: string,
   username: string,
   password: string,
-  authenticationDatabase: string
+  authenticationDatabase: string,
+  sshConnection: ConnectionParams
 }
