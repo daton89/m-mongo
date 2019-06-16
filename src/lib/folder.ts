9c5e95b27a1a34498902ef7e4444095607ed9ecd
Tony <tonydangelo123@gmail.com>
2019-06-16 18:58:48 +0200

    filter folders and add debug

--- src/lib/folder.ts
+++ src/lib/folder.ts
@@ -1,11 +1,18 @@
 import fs from 'fs';
+import debug from 'debug';
 
-export function ls(directory: string) {
+const dd = debug('folder');
+
+export function ls(directory: string): Promise<string[]> {
   return new Promise((resolve, reject) => {
     fs.readdir(directory, (err, files) => {
-      if (err) return reject(err);
-
-      resolve(files);
+      if (err) {
+        dd('err %o', err);
+        return reject(err);
+      }
+      const folders = files.filter(file => fs.lstatSync(file).isDirectory());
+      dd('folders %o', folders);
+      resolve(folders);
     });
   });
 }
