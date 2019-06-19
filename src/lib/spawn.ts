d20ca60d573002e29167b41aa28a55465d9f8054
Tony <tonydangelo123@gmail.com>
2019-06-19 22:16:36 +0200

    add logs on file, and shell option

--- src/lib/spawn.ts
+++ src/lib/spawn.ts
@@ -2,6 +2,10 @@ import { spawn } from 'child_process';
 import { Observable } from 'rxjs';
 import { Spinner } from 'clui';
 import debug from 'debug';
+import fs from 'fs';
+
+const out = fs.openSync('./out.log', 'w');
+const err = fs.openSync('./err.log', 'w');
 
 const dd = debug('spawn');
 
@@ -15,17 +19,24 @@ export default function Spawn(
   dd('full command %o', `${command} ${args.join(' ')}`);
 
   return new Observable(observer => {
-    const spawning = spawn(command, args);
+    // dd('process.env.ComSpec', process.env.ComSpec);
 
-    spawning.stdout.on('data', data => {
-      status.stop();
-      observer.next(`STDOUT :: ${data.toString()}`);
+    // write on files https://github.com/nodejs/node-v0.x-archive/issues/8795#issuecomment-65132516
+    // more info abount spawn https://www.freecodecamp.org/news/node-js-child-processes-everything-you-need-to-know-e69498fe970a/
+    const spawning = spawn(command, args, {
+      shell: true,
+      stdio: ['ignore', out, err]
     });
 
-    spawning.stderr.on('data', data => {
-      status.stop();
-      observer.next(`STDERR :: ${data.toString()}`);
-    });
+    // spawning.stdout.on('data', data => {
+    //   status.stop();
+    //   observer.next(`STDOUT :: ${data.toString()}`);
+    // });
+
+    // spawning.stderr.on('data', data => {
+    //   status.stop();
+    //   observer.next(`STDERR :: ${data.toString()}`);
+    // });
 
     spawning.on('close', code => {
       status.stop();
