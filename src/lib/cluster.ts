1880461b0dbba43225adcc0ea042591d77833422
Tony <tonydangelo123@gmail.com>
2019-06-20 14:58:59 +0200

    update cluster manager

--- src/lib/cluster.ts
+++ src/lib/cluster.ts
@@ -4,6 +4,7 @@ import debug from 'debug';
 import { ConnectionParams } from './ssh';
 import conf from './conf';
 import * as inquirer from './inquirer';
+
 const dd = debug('Cluster');
 
 export interface Cluster {
@@ -15,30 +16,59 @@ export interface Cluster {
   uri: string;
   host: string;
   ssl: string;
-  username: string;
-  password: string;
-  authenticationDatabase: string;
-  sshConnection: ConnectionParams;
+  authEnabled: boolean;
+  username?: string;
+  password?: string;
+  authenticationDatabase?: string;
+  sshConnection?: ConnectionParams;
 }
 
 export class ClusterManager {
   public static showClusters() {
     this.getClusters().forEach((cluster: Cluster, index: number) => {
+      const { cyan, magenta } = chalk;
       console.log(`${chalk.bold.cyan(`Cluster #${index}`)}
-        ${chalk.cyan('Name:')} ${chalk.magenta(cluster.name)}
-        ${chalk.cyan('Uri:')} ${chalk.magenta(cluster.uri)}
-        ${chalk.cyan('Host:')} ${chalk.magenta(cluster.host)}
-        ${chalk.cyan('SSL:')} ${chalk.magenta(cluster.ssl)}
-        ${chalk.cyan('Username:')} ${chalk.magenta(cluster.username)}
-        ${chalk.cyan('Password:')} ${chalk.magenta(cluster.password)}
-        ${chalk.cyan('authenticationDatabase:')} ${chalk.magenta(
-        cluster.authenticationDatabase
+        ${cyan('Name:')} ${magenta(cluster.name)}
+        ${cyan('Uri:')} ${magenta(cluster.uri)}
+        ${cyan('Host:')} ${magenta(cluster.host)}
+        ${cyan('SSL:')} ${magenta(cluster.ssl)}
+        ${cyan('Username:')} ${magenta(cluster.username || '')}
+        ${cyan('Password:')} ${magenta(cluster.password || '')}
+        ${cyan('authenticationDatabase:')} ${magenta(
+        cluster.authenticationDatabase || ''
       )}
       `);
     });
   }
   public static async create() {
     const cluster = await inquirer.askMongoCluster();
+
+    if (cluster.requiresSSH === 'Yes') {
+      const {
+        useExistingSshConnection
+      } = await inquirer.useExistingSshConnection();
+
+      if (useExistingSshConnection) {
+        const sshConnections: ConnectionParams[] =
+          conf.get('sshConnections') || [];
+
+        const { connectionHost } = await inquirer.selectSshConnection(
+          sshConnections
+        );
+
+        const sshConnection = sshConnections.find(
+          conn => conn.host === connectionHost
+        );
+
+        // tslint:disable-next-line: no-object-mutation
+        if (sshConnection) cluster.sshConnection = sshConnection;
+      } else {
+        const sshConnection = await inquirer.askSshConnection();
+
+        // tslint:disable-next-line: no-object-mutation
+        cluster.sshConnection = sshConnection;
+      }
+    }
 
     const clusters = conf.get('clusters') || [];
 
@@ -49,7 +79,7 @@ export class ClusterManager {
     return cluster;
   }
   public static async getCluster() {
-    const clusters = conf.get('clusters') || [];
+    const clusters = this.getClusters();
 
     if (!clusters.length) {
       const newCluster = this.create();
@@ -61,13 +91,15 @@ export class ClusterManager {
 
     const { clusterName } = await inquirer.selectCluster(clusters);
 
-    const cluster = clusters.find((c: Cluster) => c.name === clusterName);
+    const cluster = clusters.find(({ name }) => name === clusterName);
 
     dd('getCluster %o', cluster);
 
+    if (!cluster) throw new Error('Cluster not found')
+
     return cluster;
   }
-  private static getClusters() {
+  private static getClusters(): Cluster[] {
     return conf.get('clusters') || [];
   }
 }
